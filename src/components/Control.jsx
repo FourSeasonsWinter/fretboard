0bf3536fcd088cba21d701a422d04f1f82a9f1e8
Luiz Antonio <louis_worklad24@outlook.com>
2025-02-14 02:32:58 -0300

    Improve settings appearence.

--- src/components/Control.jsx
+++ src/components/Control.jsx
@@ -1,22 +1,37 @@
+import { useState } from "react";
 import "./css/control.css";
 
 export default function Control({ onAccidentalChange, onRotate }) {
+  const [visible, setVisible] = useState(false);
+
   return (
-    <div className="control">
-      <button
-        onClick={() => {
-          onAccidentalChange();
-        }}
-      >
-        ♯ / ♭
-      </button>
-      <button
-        onClick={() => {
-          onRotate();
-        }}
-      >
-        rotate
-      </button>
-    </div>
+    <>
+      <div className="control">
+        {visible && (
+          <button
+            onClick={() => {
+              onAccidentalChange();
+            }}
+          >
+            ♯ / ♭
+          </button>
+        )}
+        <img
+          className="settings-icon"
+          src="/settings.svg"
+          alt="fretboard settings"
+          onClick={() => setVisible(!visible)}
+        />
+        {visible && (
+          <button
+            onClick={() => {
+              onRotate();
+            }}
+          >
+            rotate
+          </button>
+        )}
+      </div>
+    </>
   );
 }
